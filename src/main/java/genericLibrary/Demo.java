fd9eff1c0939bfd817f7b7104c391992bcbee687
ashab <ashab@LAPTOP-E6LSSHHS>
2023-10-12 20:45:03 +0530

    DemoFileAdded_01

---
+++ src/main/java/genericLibrary/Demo.java
@@ -0,0 +1,12 @@
+package genericLibrary;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is demo file added to test Git commit");
+	}
+
+}
